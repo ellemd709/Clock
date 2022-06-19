dab8917e64f046d1ccbdfe736ccd65bdd2043603
ellemd709 <ellemd@icloud.com>
2022-06-18 21:43:29 -0230

    script edits

---
+++ script.js
@@ -0,0 +1,27 @@
+var now = new Date();
+var hours = now.getHours();
+var minutes = now.getMinutes();
+var seconds = now.getSeconds();
+var day = now.getDay();
+var date = now.getDate();
+var month = now.getMonth();
+var year = now.getFullYear();
+
+var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+hours = hours % 12 || 12;
+minutes = minutes <10 ? '0' + minutes : minutes;
+seconds = seconds <10 ? '0' + seconds : seconds;
+date = date <10 ? '0' + date : date; 
+
+var period = hours < 12? 'AM' : 'PM';
+time.innerHTML = hours + ':' + minutes + ':' + seconds + '' + period;
+dateTime.innerHTML = dayNames[day] + ',' + monthNames[month] + '' + date + ',' + year;
+
+
+
+
+
+var time = document.querySelector('.time');
+var dateTime = document.querySelector('.date-time');
